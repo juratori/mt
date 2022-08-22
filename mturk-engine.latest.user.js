801bddb472613bb3471300c942e8fb3b236a1976
Yura <yura@linux.com>
2022-08-22 11:16:56 +0000

    Add files via upload

---
+++ mturk-engine.latest.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Mturk Engine (cloned)
+// @namespace    https://github.com/juratori/mturk-engine/
+// @version      1.8.3
+// @description  Earn money more efficiently on Amazon's Mechanical Turk work platform.
+// @author       Anveio (Shovon Hasan)
+// @license      MIT
+// @match        https://worker.mturk.com/?mturkengine
+// @match        https://www.mturk.com/?mturkengine
+// @require      https://raw.githubusercontent.com/juratori/mturk-engine/main/build/mturk-engine.latest.raw.user.js
+// @grant        none
+// ==/UserScript==
+
+/**
+ * After downloading this script visit https://worker.mturk.com/?mturkengine
+ *
+ * Mturk Engine is free and open source. Visit this project's github page at: https://github.com/Anveio/mturk-engine
+ * There you can view the source code, post issues, submit changes, suggest features, and download the latest version.
+ * Changelogs are available at: https://github.com/Anveio/mturk-engine/releases
+ */
